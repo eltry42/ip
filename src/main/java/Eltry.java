8390c9ee03fd04a324d5cb174247a1e6acf90b18
Eldridge <eldridgeng181203@gmail.com>
2025-08-21 02:36:54 +0800

    Bot echos commands with exit functionality

--- src/main/java/Eltry.java
+++ src/main/java/Eltry.java
@@ -1,13 +1,40 @@
+import java.util.Scanner;
+
 public class Eltry {
     public static void main(String[] args) {
         String chatbotName = "Eltry";
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+        String logo = """
+            _____ _   _____ ______   __
+           | ____| | |_   _|  _ \\ \\ / /
+           |  _| | |   | | | |_) \\ V / 
+           | |___| |___| | |  _ < | |  
+           |_____|_____|_| |_| \\_\\|_|            
+          """;
+          
 
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm " + chatbotName);
-        System.out.println(" What can I do for you?");
+        System.out.println("Hello from\n" + logo);
+        System.out.println(" Hello! I'm " + chatbotName + ", your friendly echo bot.");
+        System.out.println(" Type anything and I'll echo it back. Type 'bye' to exit!");
         System.out.println("____________________________________________________________");
 
-        // Exit message
-        System.out.println("Bye. Hope to see you again soon!");
+        while (true) {
+            input = scanner.nextLine();
+
+            System.out.println("____________________________________________________________");
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println(" Bye! Hope to see you again soon! 😊");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println(" " + input);
+                System.out.println("____________________________________________________________");
+            }
+        }
+
+        scanner.close();
     }
 }
