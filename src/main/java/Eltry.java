3ae84e76649b4ef709ad2aa248b3a5da19a77700
Eldridge <eldridgeng181203@gmail.com>
2025-08-21 02:24:55 +0800

    Rename class and add greeting and exit message

---
+++ src/main/java/Eltry.java
@@ -0,0 +1,13 @@
+public class Eltry {
+    public static void main(String[] args) {
+        String chatbotName = "Eltry";
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm " + chatbotName);
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        // Exit message
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
